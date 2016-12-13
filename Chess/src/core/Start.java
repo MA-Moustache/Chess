ff8e0eb15bf529771f6a914a77e7b0fee62e6ea3
MA-Moustache <michael-genique@hotmail.com>
2016-12-13 15:28:48 +0100

    Possibilité de choisir la pièce à avancer

--- Chess/src/core/Start.java
+++ Chess/src/core/Start.java
@@ -1,11 +1,26 @@
 package core;
 
+import java.util.Scanner;
+
 public class Start {
 	
 	public static void main(String[] args)
 	{
+		int ligneI, colonneI, ligneF, colonneF;
+		Scanner scan = new Scanner(System.in);
 		Jeu chess = new Jeu();
-		chess.deplacer(6, 4, 5, 4);
+		
+		System.out.print("Ligne de la pièce à bouger: ");
+		ligneI = scan.nextInt();
+		System.out.print("Colonne de la pièce à bouger: ");
+		colonneI = scan.nextInt();
+		System.out.print("Ligne de la ligne ou diriger la pièce: ");
+		ligneF = scan.nextInt();
+		System.out.print("Colonne de la ligne ou diriger la pièce: ");
+		colonneF = scan.nextInt();
+
+		
+		chess.deplacer(ligneI, colonneI, ligneF, colonneF);
 		
 	}
 }
